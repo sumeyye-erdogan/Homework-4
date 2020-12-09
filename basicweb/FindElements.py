7fc0268a40e7f690127489c42dd34a2d80e35819
sumeyye-erdogan <sumeyye.erdogan@useinsider.com>
2020-12-09 11:28:44 +0300

    edit all code

--- basicweb/FindElements.py
+++ basicweb/FindElements.py
@@ -19,42 +19,50 @@ class BrowserInfo():
         driver.find_element_by_id("login-button").click()
         driver.implicitly_wait(10)
 
+        #1
         #elementbyId = driver.find_element_by_id("enableUtmSettings")
 
         #if elementbyId is not None:
-         #   print("We found an element")
+        #print("We found an element")
 
+        #2
 
         #input_SMS =driver.find_element_by_xpath("//div[@id='smsMessageAttribute']")
         #input_SMS.send_keys("")
         #print(driver.find_element_by_xpath("//p[.='The Your Message field is required.']").text)
 
-       #input_SearchValue = driver.find_element_by_xpath("//input[@id='search-value']")
+        #3
+        #input_SearchValue = driver.find_element_by_xpath("//input[@id='search-value']")
         #input_SearchValue.send_keys("test")
 
+        #4
         #radiobutton_test = driver.find_element_by_xpath("//input[@id='Test']")
         #radiobutton_test.click()
 
         #driver.find_element_by_id("maybe-later").click()
         #time.sleep(5)
 
+        #5
         #button_statistics = driver.find_elements_by_class_name("statistics")
         #button_statistics = driver.find_elements_by_css_selector(".vuetable-slot.test-link")
 
         #button_statistics[0].click()
         #time.sleep(5)
 
+        #6
         #button_delete = driver.find_element_by_class_name("in-button-wrapper.qa-button.w-1.in-button-wrapper_text-alert")
         #button_delete.click()
 
         #time.sleep(3)
 
+        #7
         #driver.find_element_by_xpath('//button[@id = "Delete"]').click()
         #driver.find_element_by_link_text("Delete").click()
-        #a = driver.find_elements_by_xpath("//p[contains(text(),'Delete')]")[1]
-        #a.click()
+        #change = driver.find_elements_by_xpath("//p[contains(text(),'Delete')]")[1]
+        #change.click()
         #time.sleep(2)
         #driver.find_element_by_class_name("textButton.textBlue").click()
+        #8
         #a = driver.find_element_by_id("sms-41")
         #time.sleep(4)
         #a.click()
@@ -64,15 +72,16 @@ class BrowserInfo():
         #time.sleep(6)
         #print(driver.find_elements_by_class_name("messageAlertBoxIcon"))
 
-        a = driver.find_element_by_class_name("btnBlue")
-        a.click()
-        time.sleep(4)
-        driver.find_element_by_class_name("inputBlock").send_keys("test1237777")
-        driver.find_element_by_class_name("textButton.textBlue").click()
-        time.sleep(5)
-        driver.find_element_by_class_name("in-button-wrapper_primary-default").click()
-        time.sleep(7)
-        print("sssss")
+        #9
+        #button = driver.find_element_by_class_name("btnBlue")
+        #button.click()
+        #time.sleep(4)
+        #driver.find_element_by_class_name("inputBlock").send_keys("test1237777")
+        #driver.find_element_by_class_name("textButton.textBlue").click()
+        #time.sleep(5)
+        #driver.find_element_by_class_name("in-button-wrapper_primary-default").click()
+        #time.sleep(7)
+
 
 
 
